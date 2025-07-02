077a40af1462e4f4a0b7e7c003648208af4cc7a3
mohammad <mohammadmer3i5678@gmail.com>
2025-07-02 08:49:18 +0300

    final commit

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,14 +1,27 @@
 
 
 
-function createDiv(){
-    for(let i = 0; i < 3; i++){
-        const button = document.createElement("button");
-        button.textContent = "button 1";
-        document.getElementById("main-movie-list").appendChild(button);
-    }
-}
 
-window.onload = function(){
-    createDiv();
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const movieList = document.getElementById("main-movie-list");
+
+    axios.get("http://localhost/assignments/cinema-backend/controllers/get_movies.php")
+        .then(response=>{
+            movieList.innerHTML = '';
+            response.data.movies.forEach(movie=> {
+                createMovieSchedule(movie,movieList);
+        });
+    });
+    
+});
+
+function createMovieSchedule(movie,movieList){
+    const dv = document.createElement('div');
+    dv.className = 'movie_card';
+    dv.innerHTML= `
+        <h2>${movie["title"]}</h2>
+        <img src="http://localhost/assignments/cinema-backend/assets/images/${movie["poster"]}">
+        `;
+    movieList.appendChild(dv);
 }
